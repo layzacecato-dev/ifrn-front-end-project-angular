d39cfccba2b162884daf90da55267947f421e9f8
Layza Cecato <layza.cecato@gmail.com>
2023-02-09 18:18:45 -0300

    Create Login Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
 import {CardModule} from 'primeng/card';
+import { LoginComponent } from './views/login/login.component';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+
+
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserAnimationsModule,
     BrowserModule,
     CardModule,
+    InputTextModule,
+    ButtonModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
