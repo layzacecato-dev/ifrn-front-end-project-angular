5e0dc94b44b71419f303920dad4b22326e3182af
Layza Cecato <layza.cecato@gmail.com>
2023-02-09 12:03:14 -0300

    PrimeNG and PrimeFlex library installation. Using the Card component and creating a responsive design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {CardModule} from 'primeng/card';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    CardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
